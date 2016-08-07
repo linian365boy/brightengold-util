c352ec6441fd72b385d3d8121f03f300ad354f33
linian365boy <linian365boy@foxmail.com>
2016-08-07 19:24:26 +0800

    feat(util): 增加邮箱校验功能

--- src/main/java/com/brightengold/util/Tools.java
+++ src/main/java/com/brightengold/util/Tools.java
@@ -137,6 +137,24 @@ public class Tools {
 		return flag;
 	}
     
+    /**
+     * 验证邮箱
+     * @param email
+     * @return
+     */
+    public static boolean checkEmail(String email){
+        boolean flag = false;
+        try{
+                String check = "^([a-z0-9A-Z]+[-|_|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+                Pattern regex = Pattern.compile(check);
+                Matcher matcher = regex.matcher(email);
+                flag = matcher.matches();
+            }catch(Exception e){
+                flag = false;
+            }
+        return flag;
+    }
+    
     public static void main(String[] args) {
     	String jsonStr = Tools.getJsonStrFromPath("webConfig.json");
     	System.out.println(jsonStr);
